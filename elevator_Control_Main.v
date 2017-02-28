56cc92468dacbafba49f41bfbf4779f07cdb4e11
Kikanye <Kikanye@users.noreply.github.com>
2017-02-28 11:15:25 -0600

    Add files via upload

--- elevator_Control_Main.v
+++ elevator_Control_Main.v
@@ -1,17 +1,18 @@
 module elevator_Control_Main(input_floor, out_request_floor, In_floor, out_current_floor, direction, condition_for_delay, iCLK, LCD_DATA, LCD_RW, LCD_EN, LCD_RS);
 
-input [3:0] input_floor;                                     // Switches SW13 to SW16 to select the floor to be input
-input iCLK;                                                  // The internal clock for PIN_Y2 to setup the delay
-output reg[6:0] out_request_floor;                           // Output to register and show the value of the requested floor on the 7 segment display (HEX6)
-reg[3:0] request_floor;                                      // Stores the floor number in binary form.
-input In_floor;                                              // The clock value assigned to the KEY3 push button 
-reg[3:0] current_floor;                                      // Stores the value of the current floor as a 4-bit binary number
-output reg[6:0] out_current_floor;                           // Output to register and show the value of the current floor on the 7 segment display (HEX4)
+input [3:0] input_floor;                               // Switches SW13 to SW16 to select the floor to be input
+input iCLK;                                           // The internal clock for PIN_Y2 to setup the delay
+output reg[6:0] out_request_floor;                    // Output to register and show the value of the requested floor on the 7 segment display (HEX6)
+reg[3:0] request_floor;                               // Stores the floor number in binary form.
+input In_floor;                                       // The clock value assigned to the KEY3 push button 
+reg[3:0] current_floor;                               // Stores the value of the current floor as a 4-bit binary number
+output reg[6:0] out_current_floor;                    // Output to register and show the value of the current floor on the 7 segment display (HEX4)
 
 
-output reg [27:0] direction;                                 // shows the direction on the seven segment displays  ad UP or UNUP(for down)
+output reg [27:0] direction;                          // shows the direction on the seven segment displays UP (when going up) or UNUP(when going down) or ON (when stationery)
 
-
+// What to show for different values of direction.
+parameter GUP=28'b1111111111111100110001000001, GDOWN=28'b0011000100000100010010000001, ON=28'b0001001000000111111111111111;
 
 ///LCD Display Inputs and outputs
 output [7:0] LCD_DATA;
@@ -24,10 +25,10 @@ output LCD_RS;
 // This converter sets up the value of the request_floor in binary based on the switch which is turned on and if the clock is pressed
 always @ (posedge In_floor) // only set value when the button KEY3 is pressed
 	casex(request_floor)
-		4'bxxxx: if(input_floor[0]) request_floor=4'b0001;     // if switch SW16 is ON set request_floor to 1 in binary
-		else if(input_floor[1]) request_floor=4'b0010;         // if switch SW15 is ON set request_floor to 2 in binary
-		else if (input_floor[2]) request_floor=4'b0011;        // if switch SW14 is ON set request_floor to 3 in binary
-		else if (input_floor[3]) request_floor=4'b0100;        // if switch SW13 is ON set request_floor to 4 in binary
+		4'bxxxx: if(input_floor[0]) request_floor=4'b0001;     								// if switch SW16 is ON set request_floor to 1 in binary
+		else if(input_floor[1]) request_floor=4'b0010;        							 	// if switch SW15 is ON set request_floor to 2 in binary
+		else if (input_floor[2]) request_floor=4'b0011;        								// if switch SW14 is ON set request_floor to 3 in binary
+		else if (input_floor[3]) request_floor=4'b0100;        								// if switch SW13 is ON set request_floor to 4 in binary
 	endcase
 		
 
@@ -60,47 +61,57 @@ always @ (current_floor)
 	
 	
 
-reg [29:0] count_clock;                                // Register to use for timing (29 bits), 10seconds
-initial count_clock=0;                                 // Initialize the vlaue of the counter to 0  
-output reg condition_for_delay;                        // Stores the value of the leftmost bit to be used for delay and in the elevator if statement block
+reg [26:0] count_clock;                                										// Register to use for timing (29 bits), 10seconds
+initial count_clock=0;                                										// Initialize the vlaue of the counter to 0  
+output reg condition_for_delay;                        										// Stores the value of the leftmost bit to be used for delay and in the elevator if statement block
 	
-always @(posedge iCLK)											 // Use the internal clolck for the edge
+	
+always @(posedge iCLK)											 										// Use the internal clolck for the edge
 begin
-	count_clock=count_clock+1'b1;								 // Increase the value of the the counter by added one each time 
-	condition_for_delay=count_clock[29];                // take the leftmost bit
-			 if (request_floor>current_floor)             // Test to see if the request value is greater than the current floor
+			 if (request_floor>current_floor)             										// Test to see if the request value is greater than the current floor
 				 begin
-				  direction=28'b1111111111111100110001000001;      //GOING UP!!!!!!!!!!!!!!!                    
+				  direction=GUP;      							                            //GOING UP!!!!!!!!!!!!!!!  
+				  
+				  count_clock=count_clock+1'b1;								 										// Increase the value of the the counter by added one each time 
+				  condition_for_delay=count_clock[26];                										// take the leftmost bit	(Part of delay setup)	
+				  
 				  if(condition_for_delay)										
 				  begin
-				  count_clock=0; //!!!!!!!!!!!!!!Might have to remove this
-				  current_floor <= current_floor+4'b1;
-				  count_clock=0;
+
+				  current_floor <= current_floor+4'b1;                                //Increase current floor by 1
+				  count_clock=0;                                                      // Reset the clock delay after each floor change
+				  
 				  end
 				 end
 		 
 			 else if(request_floor<current_floor)
 				begin
-				  direction=28'b0011000100000100010010000001;// going down
+				  direction=GDOWN;                                                     // GOING DOWN!!!!!!!!!!!!!
+				  
+				  count_clock=count_clock+1'b1;								 										// Increase the value of the the counter by added one each time 
+				  condition_for_delay=count_clock[26];                										// take the leftmost bit	(Part of delay setup)
+				  
 				  if(condition_for_delay)
 				  begin
-				  count_clock=0; //!!!!!!!!!!!!!!!Might have to remove this
-				  current_floor <= current_floor-4'b1;
-				  count_clock=0;
+				  
+				  current_floor <= current_floor-4'b1;                                //Decrease current floor by 1
+				  count_clock=0;                                                      //Reset the clock delay after each floor change
+				  
 				  end
 				end
 			 
-			 else if(request_floor == current_floor)
+			 else if(request_floor == current_floor)                                 //When the request floor is reached, just stay on
 				 begin
-				  direction=28'b0001001000000111111111111111;
+				  direction=ON;                                                       //STAY ON!!!!!!!!!!!!
 				 end
 		end
 
 		
-		
-/*-------------------------------------------------------------------------------*/
-/*!!!!!!!!!!!!!!!!!!!!!!!!LCD DISPLAY SECTION OF THE CODE!!!!!!!!!!!!!!!!!!!!!!!!*/
-/*-------------------------------------------------------------------------------*/	
+	
+	
+/*---------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!LCD DISPLAY SECTION OF THE CODE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+/*---------------------------------------------------------------------------------------------------------------------------------------------------------------*/	
 
 
 
@@ -123,7 +134,7 @@ begin
 	parameter			LUT_SIZE		=	LCD_LINE1+32+1	;	// Total size of data index 
 	
 	reg [25:0] count3;
-	reg led3;
+	reg led3;                                    // the pulse of this will be used to reset the screen during short time intervals to update the display
 	initial count3=0;	
 
 	always@(posedge iCLK) 
@@ -190,7 +201,7 @@ begin
 //	ALWAYS loop sets LUT_DATA at all time based on the LUT_INDEX value which determines intialization, line 1 or lin 2	
 	always
 	begin
-		if(direction==28'b1111111111111100110001000001) //Going up
+		if(direction==GUP)                                                            //WHAT TO DISPLAY ON THE LCD WHEN GOING UP!
 		begin		
 		case(LUT_INDEX)
 		
@@ -217,18 +228,18 @@ begin
 			LCD_LINE1+11:	LUT_DATA	<=	9'h14F;	//	O
 			LCD_LINE1+12:	LUT_DATA	<=	9'h120;	//	space
 			LCD_LINE1+13:	LUT_DATA	<=	9'h128;	//	(
-			LCD_LINE1+14:	if(request_floor==4'b0001) LUT_DATA	<=	9'h131; 
-			else if(request_floor==4'b0010) LUT_DATA	<=	9'h132; 
-			else if(request_floor==4'b0011) LUT_DATA	<=	9'h133; 
-			else if(request_floor==4'b0100) LUT_DATA	<=	9'h134;
-			else LUT_DATA	<=	9'h13A;	//	insert floor here !!!!!!!!!!!!!!!
+			LCD_LINE1+14:	if(request_floor==4'b0001) LUT_DATA	<=	9'h131;       //If the vlaue of the request_floor is 1 in binary then display 1 on the LCD
+			else if(request_floor==4'b0010) LUT_DATA	<=	9'h132;                //If the vlaue of the request_floor is 2 in binary then display 2 on the LCD
+			else if(request_floor==4'b0011) LUT_DATA	<=	9'h133;                //If the vlaue of the request_floor is 3 in binary then display 3 on the LCD
+			else if(request_floor==4'b0100) LUT_DATA	<=	9'h134;                //If the vlaue of the request_floor is 4 in binary then display 4 on the LCD
+			else LUT_DATA	<=	9'h13A;	                                         //If request_floor has no vlaue, then display :
 			LCD_LINE1+15:	LUT_DATA	<=	9'h129;	//	)
 			
 //	Change Line
 			LCD_CH_LINE:	LUT_DATA	<=	9'h0C0;
 			
 //	Line 2
-//	 Changed to ECE:2220 Altera DE2 Board 
+ 
 
 			LCD_LINE2+0:	LUT_DATA	<=	9'h120;	//	space
 			LCD_LINE2+1:	LUT_DATA	<=	9'h141;	// A
@@ -241,11 +252,11 @@ begin
 			LCD_LINE2+8:	LUT_DATA	<=	9'h152;	//	R 
 			LCD_LINE2+9:	LUT_DATA	<=	9'h120;	//	space
 			LCD_LINE2+10:	LUT_DATA	<=	9'h128;	//	(
-			LCD_LINE2+11:	if(current_floor==4'b0001) LUT_DATA	<=	9'h131; 
-			else if(current_floor==4'b0010) LUT_DATA	<=	9'h132; 
-			else if(current_floor==4'b0011) LUT_DATA	<=	9'h133; 
-			else if(current_floor==4'b0100) LUT_DATA	<=	9'h134;
-			else LUT_DATA	<=	9'h13A; //	insert the number here!!!!!!!!!!!!!!
+			LCD_LINE2+11:	if(current_floor==4'b0001) LUT_DATA	<=	9'h131;       //If the value of the current_floor is 1 in binary then display 1 on the LCD
+			else if(current_floor==4'b0010) LUT_DATA	<=	9'h132;                //If the value of the current_floor is 2 in binary then display 2 on the LCD 
+			else if(current_floor==4'b0011) LUT_DATA	<=	9'h133;                //If the value of the current_floor is 3 in binary then display 3 on the LCD
+			else if(current_floor==4'b0100) LUT_DATA	<=	9'h134;                //If the value of the current_floor is 4 in binary then display 4 on the LCD
+			else LUT_DATA	<=	9'h13A;                                           //If current_floor has no value, then display :
 			LCD_LINE2+12:	LUT_DATA	<=	9'h129;	//	)
 			LCD_LINE2+13:	LUT_DATA	<=	9'h120;	//	space
 			LCD_LINE2+14:	LUT_DATA	<=	9'h13A;	//	:
@@ -253,7 +264,7 @@ begin
 		endcase
 	end
 	
-	else if(direction==28'b0011000100000100010010000001) //going down
+	else if(direction==GDOWN)                                                             //WHAT TO DISPLAY ON THE LCD WHEN GOING DOWN
 	begin
 			case(LUT_INDEX)
 //			Initial - note highest order bit is a "0" for command
@@ -280,17 +291,17 @@ begin
 			LCD_LINE1+12:	LUT_DATA	<=	9'h154;	//	T
 			LCD_LINE1+13:	LUT_DATA	<=	9'h14F;	//	O
 			LCD_LINE1+14:	LUT_DATA	<=	9'h120;	//	space
-			LCD_LINE1+15:	if(request_floor==4'b0001) LUT_DATA	<=	9'h131; 
-			else if(request_floor==4'b0010) LUT_DATA	<=	9'h132; 
-			else if(request_floor==4'b0011) LUT_DATA	<=	9'h133; 
-			else if(request_floor==4'b0100) LUT_DATA	<=	9'h134;
-			else LUT_DATA	<=	9'h13A;	//	insert floor here !!!!!!!!!!!!!!!
+			LCD_LINE1+15:	if(request_floor==4'b0001) LUT_DATA	<=	9'h131;             //If the value of the request_floor is 1 in binary then display 1 on the LCD
+			else if(request_floor==4'b0010) LUT_DATA	<=	9'h132;                      //If the value of the request_floor is 2 in binary then display 2 on the LCD
+			else if(request_floor==4'b0011) LUT_DATA	<=	9'h133;                      //If the value of the request_floor is 3 in binary then display 3 on the LCD
+			else if(request_floor==4'b0100) LUT_DATA	<=	9'h134;                      //If the value of the request_floor is 4 in binary then display 4 on the LCD
+			else LUT_DATA	<=	9'h13A;	                                                // if request_floor has no value, then display :
 			
 //	Change Line
 			LCD_CH_LINE:	LUT_DATA	<=	9'h0C0;
 			
 //	Line 2
-//	 Changed to ECE:2220 Altera DE2 Board 
+ 
 
 			LCD_LINE2+0:	LUT_DATA	<=	9'h120;	//	space
 			LCD_LINE2+1:	LUT_DATA	<=	9'h141;	// A
@@ -303,11 +314,11 @@ begin
 			LCD_LINE2+8:	LUT_DATA	<=	9'h152;	//	R 
 			LCD_LINE2+9:	LUT_DATA	<=	9'h120;	//	space
 			LCD_LINE2+10:	LUT_DATA	<=	9'h128;	//	(
-			LCD_LINE2+11:	if(current_floor==4'b0001) LUT_DATA	<=	9'h131; 
-			else if(current_floor==4'b0010) LUT_DATA	<=	9'h132; 
-			else if(current_floor==4'b0011) LUT_DATA	<=	9'h133; 
-			else if(current_floor==4'b0100) LUT_DATA	<=	9'h134;
-			else LUT_DATA	<=	9'h13A; //	insert the number here!!!!!!!!!!!!!!
+			LCD_LINE2+11:	if(current_floor==4'b0001) LUT_DATA	<=	9'h131;          //If the value of current_floor is 1 in binary, then display 1 on the LCD
+			else if(current_floor==4'b0010) LUT_DATA	<=	9'h132;                   //If the value of current_floor is 2 in binary, then display 2 on the LCD
+			else if(current_floor==4'b0011) LUT_DATA	<=	9'h133;                   //If the value of current_floor is 3 in binary, then display 3 on the LCD
+			else if(current_floor==4'b0100) LUT_DATA	<=	9'h134;                   //If the value of current_floor is 4 in binary, then display 4 on the LCD
+			else LUT_DATA	<=	9'h13A;                                              //if the current_floor has no vlaue, then display :
 			LCD_LINE2+12:	LUT_DATA	<=	9'h129;	//	)
 			LCD_LINE2+13:	LUT_DATA	<=	9'h120;	//	space
 			LCD_LINE2+14:	LUT_DATA	<=	9'h13A;	//	:
@@ -315,7 +326,7 @@ begin
 		endcase
 	end
 	
-	else if(direction== 28'b0001001000000111111111111111) // stay on
+	else if(direction==ON)                                                              // WHAT TO DISPLAY ON STAY ON
 	 begin
 			case(LUT_INDEX)
 //			Initial - note highest order bit is a "0" for command
@@ -338,11 +349,11 @@ begin
 			LCD_LINE1+8:	LUT_DATA	<=	9'h152;	//	R 
 			LCD_LINE1+9:	LUT_DATA	<=	9'h120;	//	space
 			LCD_LINE1+10:	LUT_DATA	<=	9'h128;	//	(
-			LCD_LINE1+11:	if(current_floor==4'b0001) LUT_DATA	<=	9'h131; 
-			else if(current_floor==4'b0010) LUT_DATA	<=	9'h132; 
-			else if(current_floor==4'b0011) LUT_DATA	<=	9'h133; 
-			else if(current_floor==4'b0100) LUT_DATA	<=	9'h134;
-			else LUT_DATA	<=	9'h13A; //	insert the number here!!!!!!!!!!!!!!
+			LCD_LINE1+11:	if(current_floor==4'b0001) LUT_DATA	<=	9'h131;             //If the value of current_floor is 1 in binary then display 1 on the LCD
+			else if(current_floor==4'b0010) LUT_DATA	<=	9'h132;                      //If the vlaue of current_floor is 2 in binary then display 2 on the LCD
+			else if(current_floor==4'b0011) LUT_DATA	<=	9'h133;                      //If the vlaue of current_floor is 3 in binary then display 3 on the LCD
+			else if(current_floor==4'b0100) LUT_DATA	<=	9'h134;                      //If the vlaue of current_floor is 4 in binary then display 4 on the LCD
+			else LUT_DATA	<=	9'h13A;                                                 //If current_floor has no value, then display :
 			LCD_LINE1+12:	LUT_DATA	<=	9'h129;	//	)
 			LCD_LINE1+13:	LUT_DATA	<=	9'h120;	//	space
 			LCD_LINE1+14:	LUT_DATA	<=	9'h13A;	//	:
@@ -352,7 +363,7 @@ begin
 			LCD_CH_LINE:	LUT_DATA	<=	9'h0C0;
 			
 //	Line 2
-//	 Changed to ECE:2220 Altera DE2 Board 
+
 
 			LCD_LINE2+0:	LUT_DATA	<=	9'h145;	// E
 			LCD_LINE2+1:	LUT_DATA	<=	9'h143;	// C
@@ -373,6 +384,8 @@ begin
 		endcase
 	end
 end
+
+// Call to the module to control the LCD
 
 LCD_Controller u0	(	
 
@@ -395,7 +408,12 @@ LCD_Controller u0	(
 endmodule
 
 
+/*---------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! MODULE TO CONTROL THE LCD!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+/*---------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
+
+//LCD controller module
 module LCD_Controller (	
 
 //	Host Side
